428b6a81bbe1a38d6450977244b0ca2b80246e44
adityamohite0106 <aadityamohite4973@gmail.com>
2024-11-11 09:20:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import UpperCaseTextbox from "./component/uppercase";
+
+function App() {
+  return (
+    <>
+   < UpperCaseTextbox />
+   
+    </>
+  );
+  
+}
+
+export default App;
+
+
+// cd C:\Users\hp\Desktop\Uppercase_JSX\my-app
+// npm start
